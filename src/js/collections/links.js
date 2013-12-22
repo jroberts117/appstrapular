ca5e04ab875289e4a9c4f674c854c45b766b5548
Maurice Williams <morficus@gmail.com>
2013-12-22 02:51:12 -0500

    cleaning up the view: moving dummy-data creation out of the view and in to the collection

--- src/js/collections/links.js
+++ src/js/collections/links.js
@@ -5,9 +5,50 @@ define([
 
 	mv.collections.Links = Backbone.Collection.extend({
 
-		initialize: function () {},
+		initialize: function () {
+			this.generateLinks();
+		},
 
-		model: Link
+		model: Link,
+
+		/**
+		 * Generate some sample/dummy data
+		 */
+		generateLinks: function() {
+			var link1, link2;
+
+			link1 = new Link({
+				url: 'http://www.google.com',
+				display: 'Google',
+				target: '_blank'
+			});
+
+			link2 = new Link({
+				url: 'http://www.github.com',
+				display: 'github',
+				target: '_blank'
+			});
+
+			this.add(link1);
+			this.add(link2);
+
+			//alternate way of defining the dummy data
+			/*
+			var links = [
+				{
+					url: 'http://www.google.com',
+					display: 'Google',
+					target: '_blank'
+				},
+				{
+					url: 'http://www.github.com',
+					display: 'github',
+					target: '_blank'
+				}
+			];
+			this.add(links);
+			*/
+		}
 
 	});
 
