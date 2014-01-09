1dab24e017cf1a0e4bdca850d1b7305970bb8c79
jroberts117 <jeff.roberts117@gmail.com>
2014-01-09 13:20:51 -0500

    Added comments to link test
    Formatted codebase and added comments to link test

--- test/specs/linkSpec.js
+++ test/specs/linkSpec.js
@@ -1,6 +1,8 @@
 define(['core', 'collections/links', 'sinon'], function(core, LinkCollection) {
 
+	/**Sample of nested testing and sinon.fakeServer. This shows how the fake server can be used to simulate network requests, and how tests can be nested to build on the setups of each other. In this scenario, the outer setup takes care of orchestrating the fake server and the inner setup prepopulates the collection using that fake server.*/
 	describe("Link Collection Tests", function() {
+		/**Mock object*/
 		var fixtureResponse = [{
 			url : 'www.google.com',
 			display : 'self',
@@ -15,9 +17,12 @@ define(['core', 'collections/links', 'sinon'], function(core, LinkCollection) {
 			target : 'self'
 		}];
 
+		/** Before each test, create a link collection object for the test, and then set up a mock sinon server that will respond to fetch requests with the mock object above*/
 		beforeEach(function() {
 			this.linkCollection = new LinkCollection();
+			// Create the server
 			this.server = sinon.fakeServer.create();
+			// Set the default response for requesting a collection of links
 			this.server.respondWith("GET", "fakeserver/getlinks", [200, {
 				'Content-Type' : 'application/json'
 			}, JSON.stringify(fixtureResponse)]);
@@ -29,12 +34,14 @@ define(['core', 'collections/links', 'sinon'], function(core, LinkCollection) {
 
 		it('fetch succeeds', function() {
 			this.linkCollection.fetch();
+			// Fetch request will have queued with our fake server so now we need to tell the server to respond
 			this.server.respond();
 			expect(this.linkCollection.at(0).get('url')).toEqual(fixtureResponse[0].url);
 		});
 
 		describe('Post Fetch Processing', function() {
 
+			/** Inner tests are all to be predicated on the object being populated, so as part of the setup we can prepopulate our collection using the server created in the outer tests*/
 			beforeEach(function() {
 				this.linkCollection.fetch();
 				this.server.respond();
